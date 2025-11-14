ac1d88b6875c83b81b9cff7913c7e946fd3243a4
Kufiet <kufietajakub@gmail.com>
2025-11-14 04:33:18 +0100

    Made a list of notes(only front, no logic behind)

--- client/src/Pages/AddTicket.js
+++ client/src/Pages/AddTicket.js
@@ -1,6 +1,7 @@
 import '../css/Menu.css';
 import CreateMenu from '../components/menu';
 import styles from '../css/modules/addTicket.module.css'
+import MyButton from '../components/buttons';
 
 function AddTicket() {
     return (
@@ -11,7 +12,7 @@ function AddTicket() {
            <div className={styles.content}>
                 <div className={`${styles.card} ${styles.tickets}`}>
                     <div className={`${styles.inputs}`}>
-                      Id:<br/>
+                      Ticket id:<br/>
                       <input type='text'></input><br/>
                       <br/>
                       <b>Client information:</b><br/>
@@ -46,7 +47,9 @@ function AddTicket() {
                       <button>Clear Ticket</button>
                     </div>
                     <div className={styles.rightTicket}>
+                    Notes:
                       <div className={styles.tableWrapper}>
+                        
                         <table>
                           <thead>
                             <tr>
@@ -75,8 +78,74 @@ function AddTicket() {
                               <td>2025-11-10 13:05</td>
                               <td>Problem rozwiązany – konto odblokowane.</td>
                             </tr>
+                            <tr>
+                              <td>1</td>
+                              <td>Anna Kowalska</td>
+                              <td>2025-11-10 10:15</td>
+                              <td>Klient zgłosił problem z logowaniem.</td>
+                            </tr>
+                            <tr>
+                              <td>2</td>
+                              <td>Piotr Nowak</td>
+                              <td>2025-11-10 11:40</td>
+                              <td>Sprawa przekazana do działu IT.</td>
+                            </tr>
+                            <tr>
+                              <td>3</td>
+                              <td>Agnieszka Lewandowska</td>
+                              <td>2025-11-10 13:05</td>
+                              <td>Problem rozwiązany – konto odblokowane.</td>
+                            </tr>
+                            <tr>
+                              <td>1</td>
+                              <td>Anna Kowalska</td>
+                              <td>2025-11-10 10:15</td>
+                              <td>Klient zgłosił problem z logowaniem.</td>
+                            </tr>
+                            <tr>
+                              <td>2</td>
+                              <td>Piotr Nowak</td>
+                              <td>2025-11-10 11:40</td>
+                              <td>Sprawa przekazana do działu IT.</td>
+                            </tr>
+                            <tr>
+                              <td>3</td>
+                              <td>Agnieszka Lewandowska</td>
+                              <td>2025-11-10 13:05</td>
+                              <td>Problem rozwiązany – konto odblokowane.</td>
+                            </tr>
+                            <tr>
+                              <td>1</td>
+                              <td>Anna Kowalska</td>
+                              <td>2025-11-10 10:15</td>
+                              <td>Klient zgłosił problem z logowaniem.</td>
+                            </tr>
+                            <tr>
+                              <td>2</td>
+                              <td>Piotr Nowak</td>
+                              <td>2025-11-10 11:40</td>
+                              <td>Sprawa przekazana do działu IT.</td>
+                            </tr>
+                            <tr>
+                              <td>3</td>
+                              <td>Agnieszka Lewandowska</td>
+                              <td>2025-11-10 13:05</td>
+                              <td>Problem rozwiązany – konto odblokowane.</td>
+                            </tr>
                           </tbody>
                         </table>
+                      </div>
+                      Add work info:<br/>
+                      Note:<br/>
+                      <div className={styles.notes}>     
+                        <textarea className={styles.inputNotes}></textarea> 
+                        <div className={styles.buttonsSection}>
+                        <MyButton
+                          text="Add note"
+                          width={"100px"}
+                          margin={20}
+                          />
+                        </div>
                       </div>
                     </div>
 
